e648fa69ee015174c8869029d275f55015702c45
sofiaoreis <sofiareis1994@gmail.com>
2021-08-16 14:16:17 +0100

    updating meta-data

--- website/src/App.js
+++ website/src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
         <a
           className="App-link"
